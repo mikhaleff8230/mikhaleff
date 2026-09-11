9c1f8a5a757ec6500c35a22746a6cce8e0ebd514
Alexander Mikhaleff <mikhaleff8230@users.noreply.github.com>
2026-09-11 21:44:53 +0300

    Register external Sanity Studio

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,6 +4,9 @@ export default defineCliConfig({
   project: {
     basePath: "/studio",
   },
+  deployment: {
+    appId: "h1276xaq45rqrnk08wt0rv5d",
+  },
   api: {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "replace-me",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
