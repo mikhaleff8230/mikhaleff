f7f587314d039a2a8de88baf12ae9f71ccec7284
Alexander Mikhaleff <mikhaleff8230@users.noreply.github.com>
2026-09-11 21:37:46 +0300

    Connect Sanity Studio

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,6 +1,9 @@
 import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
+  project: {
+    basePath: "/studio",
+  },
   api: {
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? "replace-me",
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? "production",
